78b5cc81ee041d9d41ac6f95289d1b540a13f722
tanishqa321 <tender.tanishka@gmail.com>
2025-01-08 02:49:49 +0530

    Added refractor:mood analyser constructor

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        // Using the parameterized constructor
+        MoodAnalyser moodAnalyser1 = new MoodAnalyser("I am in Sad Mood");
+        String result1 = moodAnalyser1.analyseMood();
+        System.out.println("Message: " + moodAnalyser1.getMessage() + " | Mood: " + result1);
 
-        // Test Case 1.1
-        String message1 = "I am in Sad Mood";
-        String result1 = moodAnalyser.analyseMood(message1);
-        System.out.println("Message: " + message1 + " | Mood: " + result1);
-
-        // Test Case 1.2
-        String message2 = "I am in Any Mood";
-        String result2 = moodAnalyser.analyseMood(message2);
-        System.out.println("Message: " + message2 + " | Mood: " + result2);
+        // Using the default constructor and setting the message later
+        MoodAnalyser moodAnalyser2 = new MoodAnalyser();
+        moodAnalyser2.setMessage("I am in Any Mood");
+        String result2 = moodAnalyser2.analyseMood();
+        System.out.println("Message: " + moodAnalyser2.getMessage() + " | Mood: " + result2);
 
     }
 }
--- src/MoodAnalyser.java
+++ src/MoodAnalyser.java
@@ -1,6 +1,27 @@
 public class MoodAnalyser {
-    public String analyseMood(String message) {
-        if (message.toLowerCase().contains("sad")) {
+    private String message;
+
+    // Default constructor
+    public MoodAnalyser() {
+    }
+
+    // Parameterized constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    // Getter for message
+    public String getMessage() {
+        return message;
+    }
+
+    // Setter for message
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if (message != null &&  message.toLowerCase().contains("sad")) {
             return "SAD";
         } else {
             return "HAPPY";
